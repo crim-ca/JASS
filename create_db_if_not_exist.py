39642628a1de9caee5c2243e03e6bf37d670584f
Jean-François Héon <Jean-Francois.Heon@crim.ca>
2017-05-25 10:17:55 -0400

    new configparser syntax

--- create_db_if_not_exist.py
+++ create_db_if_not_exist.py
@@ -9,25 +9,27 @@ import subprocess
 import time
 from pymongo.errors import ConnectionFailure
 
+
 # Will also create an index if not none.
-def createCollIfNotExist(db,collName,index):
+def createCollIfNotExist(db, collName, index):
     coll = None
-    if db.system.namespaces.find({"name" : {"$regex" : "{0}.{1}".format(db,collName)}}) == 0:
+    if db.system.namespaces.find({"name": {"$regex": "{0}.{1}".format(db, collName)}}) == 0:
         coll = db.create_collection(collName)
     else:
         coll = db[collName]
-    if index != None :
+    if index != None:
         coll.create_index(index)
     return coll
 
+
 def createDbIfNotExist(config_path):
     # print "Reading config from {0}".format(config_path)
-    SETTINGS = configparser.Safeconfigparser()
+    SETTINGS = configparser.ConfigParser()
     SETTINGS.read(config_path)
 
     retryTimes = 0
     MC = None
-    while(retryTimes < 3):
+    while (retryTimes < 3):
         try:
             retryTimes = retryTimes + 1
             mongo_host = ""
@@ -36,7 +38,7 @@ def createDbIfNotExist(config_path):
             else:
                 mongo_host = SETTINGS.get("ServiceStockageAnnotations", "MONGO_HOST")
 
-            MC = MongoClient(mongo_host,int(SETTINGS.get("ServiceStockageAnnotations","MongoPort")))
+            MC = MongoClient(mongo_host, int(SETTINGS.get("ServiceStockageAnnotations", "MongoPort")))
 
             dbName = SETTINGS.get("ServiceStockageAnnotations", "MongoDb")
             db = MC[(dbName)]
@@ -55,11 +57,12 @@ def createDbIfNotExist(config_path):
             break
         except Exception as e:
             # print "Connection to mongo failed. Retrying in 10 seconds. Reason: {0}".format(str(e))
-            #print "Connection to mongo failed. Retrying in 10 seconds"
+            # print "Connection to mongo failed. Retrying in 10 seconds"
             time.sleep(10)
 
+
 if __name__ == "__main__":
-	if len(sys.argv) > 1:
-		createDbIfNotExist(sys.argv[1])
-	else:
-		createDbIfNotExist(os.environ['JASS_CONFIG_PATH'])
+    if len(sys.argv) > 1:
+        createDbIfNotExist(sys.argv[1])
+    else:
+        createDbIfNotExist(os.environ['JASS_CONFIG_PATH'])
