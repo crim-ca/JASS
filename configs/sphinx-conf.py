da1e075e3dcf002f5ac81025f546272a290073be
Jean-François Héon <Jean-Francois.Heon@crim.ca>
2017-05-25 15:52:57 -0400

    2to3

--- configs/sphinx-conf.py
+++ configs/sphinx-conf.py
@@ -51,8 +51,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Annotation Storage'
-copyright = u'2014, Anton Zakharov'
+project = 'Annotation Storage'
+copyright = '2014, Anton Zakharov'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -204,8 +204,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'AnnotationStorage.tex', u'Annotation Storage Documentation',
-   u'Anton Zakharov', 'manual'),
+  ('index', 'AnnotationStorage.tex', 'Annotation Storage Documentation',
+   'Anton Zakharov', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -234,8 +234,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'annotationstorage', u'Annotation Storage Documentation',
-     [u'Anton Zakharov'], 1)
+    ('index', 'annotationstorage', 'Annotation Storage Documentation',
+     ['Anton Zakharov'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -248,8 +248,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'AnnotationStorage', u'Annotation Storage Documentation',
-   u'Anton Zakharov', 'AnnotationStorage', 'One line description of project.',
+  ('index', 'AnnotationStorage', 'Annotation Storage Documentation',
+   'Anton Zakharov', 'AnnotationStorage', 'One line description of project.',
    'Miscellaneous'),
 ]
 
